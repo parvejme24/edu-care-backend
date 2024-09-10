57e3addb54bcd33b0c7d8c828e3c1131b3c05e4d
Md Parvej <mdparvejme24@gmail.com>
2024-09-10 21:57:09 +0600

    added anotehr two router

--- src/routers/CollegeRouter.js
+++ src/routers/CollegeRouter.js
@@ -8,5 +8,8 @@ collegeRouter.get('/colleges', collegeController.getAllColleges);
 collegeRouter.get('/colleges/:id', collegeController.getCollegeById);
 collegeRouter.put('/colleges/:id', collegeController.updateCollege);
 collegeRouter.delete('/colleges/:id', collegeController.deleteCollege);
+collegeRouter.get('/colleges/search', collegeController.searchCollege);
+collegeRouter.get('/colleges/top-rated', collegeController.getTopRatedColleges);
+
 
 module.exports = collegeRouter;
